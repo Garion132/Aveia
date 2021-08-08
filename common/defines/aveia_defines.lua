b5dc2c192ac67552e804034a95d357448d255e1a
Strategy-Gamer <37280112+Strategy-Gamer@users.noreply.github.com>
2021-08-08 04:05:10 -0400

    Defines, Airships, Development Rework

--- common/defines/aveia_defines.lua
+++ common/defines/aveia_defines.lua
@@ -1,2 +1,21 @@
-NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
 NDefines.NGame.START_DATE = "1272.8.6"
+
+NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
+
+NDefines.NMilitary.INF_REGIMENT_MANPOWER_COST = 0.5 -- 500 Men in an infantry regiment
+NDefines.NMilitary.CAV_REGIMENT_MANPOWER_COST = 0.2 -- 200 Men in an airship regiment
+NDefines.NMilitary.ART_REGIMENT_MANPOWER_COST = 0.5 -- 500 Men in an arty regiment
+
+NDefines.NMilitary.CAVALRY_COST = 100.0 -- Airships are very expensive
+NDefines.NMilitary.CAVALRY_TIME = 730 -- and take a long time to build
+NDefines.NMilitary.CAV_LOOT = 0.1
+NDefines.NMilitary.CAV_SUPPRESSION = 0.1 -- but are less effective as supression b/c not as many men
+NDefines.NMilitary.CAVALRY_SPEED = 0.8 -- and are slightly faster
+
+NDefines.NMilitary.BASE_COMBAT_WIDTH = 10
+NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 1.5
+NDefines.NMilitary.MONTHLY_REINFORCE = 0.05
+NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.1
+
+NDefines.NAI.MIN_CAV_PERCENTAGE = 0
+NDefines.NAI.MAX_CAV_PERCENTAGE = 20
