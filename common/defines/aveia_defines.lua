1e5afe35faa4415d2c839b9e36f28478a9939956
Strategy-Gamer <37280112+Strategy-Gamer@users.noreply.github.com>
2022-03-10 16:03:56 -0500

    changing unit strengths back

--- common/defines/aveia_defines.lua
+++ common/defines/aveia_defines.lua
@@ -4,9 +4,9 @@ NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
 NDefines.NCountry.FREE_IDEA_GROUP_COST = 7
 NDefines.NCountry.PS_BUY_IDEA = 300
 
-NDefines.NMilitary.INF_REGIMENT_MANPOWER_COST = 0.5 -- 500 Men in an infantry regiment
-NDefines.NMilitary.CAV_REGIMENT_MANPOWER_COST = 0.2 -- 200 Men in an airship regiment
-NDefines.NMilitary.ART_REGIMENT_MANPOWER_COST = 0.5 -- 500 Men in an arty regiment
+NDefines.NMilitary.INF_REGIMENT_MANPOWER_COST = 1 -- 500 Men in an infantry regiment
+NDefines.NMilitary.CAV_REGIMENT_MANPOWER_COST = 0.5 -- 200 Men in an airship regiment
+NDefines.NMilitary.ART_REGIMENT_MANPOWER_COST = 1 -- 500 Men in an arty regiment
 
 NDefines.NMilitary.CAVALRY_COST = 100.0 -- Airships are very expensive
 NDefines.NMilitary.CAVALRY_TIME = 730 -- and take a long time to build
@@ -15,12 +15,11 @@ NDefines.NMilitary.CAV_SUPPRESSION = 0.1 -- but are less effective as supression
 NDefines.NMilitary.CAVALRY_SPEED = 0.8 -- and are slightly faster
 
 NDefines.NMilitary.BASE_COMBAT_WIDTH = 10
-NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 1.5
-NDefines.NMilitary.MONTHLY_REINFORCE = 0.05
-NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.1
+NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.2
 
 NDefines.NAI.MIN_CAV_PERCENTAGE = 0
 NDefines.NAI.MAX_CAV_PERCENTAGE = 20
+NDefines.NAI.ARTILLERY_FRACTION = 0.3
 NDefines.NCountry.MAX_PROVINCES_FOR_FREE_CITY = 2		-- A free city cannot have more provinces than this
 NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 5	
 NDefines.NDiplomacy.HRE_PRINCE_AUTHORITY_THRESHOLD = 17
